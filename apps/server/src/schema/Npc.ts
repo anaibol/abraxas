41b19b3c00033d4d75ef0cec6d85d68ed772fb26
Anibal <juananibal.micheli@gmail.com>
2026-02-17 15:38:26 -0300

    Update Npc.ts

--- apps/server/src/schema/Npc.ts
+++ apps/server/src/schema/Npc.ts
@@ -11,7 +11,8 @@ export class Npc extends Schema {
   @type("boolean") alive: boolean = true;
   @type("uint8") str: number = 0;
   @type("uint8") agi: number = 0;
-  @type("uint8") intStat: number = 0; // Using intStat to match Player schema naming if possible, or just int
+  @type("uint8") intStat: number = 0;
+  @type("int16") mana: number = 0;
   @type("boolean") stealthed: boolean = false;
   @type("boolean") stunned: boolean = false;
   
