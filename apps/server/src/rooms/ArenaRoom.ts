e1908016b89031aa6d368b3935b2ac213b046a57
Anibal <juananibal.micheli@gmail.com>
2026-02-20 04:32:48 -0300

    Update ArenaRoom.ts

--- apps/server/src/rooms/ArenaRoom.ts
+++ apps/server/src/rooms/ArenaRoom.ts
@@ -69,7 +69,7 @@ export class ArenaRoom extends Room<{ state: GameState }> {
 	private bankSystem!: BankSystem;
 
 	private broadcastMessage: BroadcastFn = (type, data, options) => {
-		this.broadcast(type as string, data, options as Parameters<Room["broadcast"]>[2]);
+		this.broadcast(String(type), data, options);
 	};
 
 	async onCreate(options: JoinOptions & { mapName?: string }) {
